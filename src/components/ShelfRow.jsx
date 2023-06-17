6ddb40caa634dd99bfc537a7c181b3f173fe05d6
Abin John <abinjohnanto@gmail.com>
2023-06-17 20:54:10 +0530

    feat: add no books text

--- src/components/ShelfRow.jsx
+++ src/components/ShelfRow.jsx
@@ -6,6 +6,9 @@ const ShelfRow = ({ title, books }) => {
   return (
     <div className="shelf-row">
       <h2 className="row-title">{title}</h2>
+      {books.length === 0 && (
+        <p className="no-books">No books in this category!</p>
+      )}
       <div className="row-books">
         {books.map((book) => (
           <BookCard key={book.id} book={book} />
